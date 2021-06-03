31b5b0c875863bb1694e7bc763a1fb0bbc79d0fe
gh <22863750+rolypolyvg295@users.noreply.github.com>
2021-06-03 02:20:03 +0000

    [gj] Change symbol accr to ignore pad + id2str to stop at <eos>

--- gj/code/metrics.py
+++ gj/code/metrics.py
@@ -1,5 +1,6 @@
 import editdistance
 import numpy as np
+import torch
 
 def word_error_rate(predicted_outputs, ground_truths):
 	sum_wer=0.0
@@ -18,4 +19,11 @@ def sentence_acc(predicted_outputs, ground_truths):
 		if np.array_equal(output,ground_truth):
 			correct_sentences+=1
 	return correct_sentences
+
+def correct_symbol(predictions, expected):
+	mask = expected != -1
+	correct = predictions == expected
+	using = correct & mask
+	ret = torch.sum(using, dim=(0, 1)).item()
+	return ret
 
